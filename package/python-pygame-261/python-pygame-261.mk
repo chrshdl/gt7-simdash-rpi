77f61c2017807553141473c35d7024925b2a0997
chrshdl <cwasilei@gmail.com>
2025-08-08 20:24:00 +0200

    add pygame 2.6.1 with SDL2 support

---
+++ package/python-pygame-261/python-pygame-261.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# python-pygame-261
+#
+################################################################################
+
+PYTHON_PYGAME_261_VERSION = 2.6.1
+# PyPI stores the pygame sdist at this location. The archive name uses
+# the "pygame" prefix and version number.
+PYTHON_PYGAME_261_SOURCE = pygame-$(PYTHON_PYGAME_261_VERSION).tar.gz
+PYTHON_PYGAME_261_SITE = https://files.pythonhosted.org/packages/source/p/pygame
+
+# Pygame uses the LGPL-2.1+ license.  See the LICENSE file in the
+# upstream source distribution for details.
+PYTHON_PYGAME_261_LICENSE = LGPL-2.1+
+PYTHON_PYGAME_261_LICENSE_FILES = LGPL
+
+# Use setuptools/pep517 build backend. Pygame switched to a modern
+# packaging approach so distutils is no longer available.  The
+# Buildroot python package infrastructure will invoke the appropriate
+# build system.
+PYTHON_PYGAME_261_SETUP_TYPE = pep517
+
+# Build dependencies.  Pygame requires SDL2 and its image/font
+# extension libraries as well as libpng, libjpeg and freetype.  These
+# selections ensure the module is built with full multimedia support.
+PYTHON_PYGAME_261_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf libpng libjpeg freetype
+
+# Evaluate the generic Python package build infrastructure.
+$(eval $(python-package))
